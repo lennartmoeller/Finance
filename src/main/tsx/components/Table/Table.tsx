9734b4cecdc57bd0f945254648ae800810980c10
Lennart Möller <kontakt@lennartmoeller.com>
2024-08-28 15:10:55 +0200

    Add pre row and post row to table component

--- src/main/tsx/components/Table/Table.tsx
+++ src/main/tsx/components/Table/Table.tsx
@@ -6,16 +6,20 @@ interface TableProps<T> {
     data: Array<T>;
     header: ReactNode;
     body: (element: T, index: number) => ReactNode;
+    preRow?: ReactNode;
+    postRow?: ReactNode;
 }
 
-const Table = <T, >({data, header, body}: TableProps<T>) => {
+const Table = <T, >({data, header, body, preRow, postRow}: TableProps<T>) => {
     return (
         <StyledTable>
             <thead>
             {header}
             </thead>
             <tbody>
+            {preRow}
             {data.map((element: T, index: number) => body(element, index))}
+            {postRow}
             </tbody>
         </StyledTable>
     );
