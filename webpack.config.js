9e02b7956aef9f1db9dc2e19c323cba0bbdd5a7e
Lennart Möller <kontakt@lennartmoeller.com>
2024-10-22 09:56:04 +0200

    Add development mode to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,76 +1,77 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-    mode: 'development',
-    entry: './src/main/tsx/index.tsx',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-        alias: {
-            '@': path.resolve(__dirname, 'src/main/tsx')
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react'],
-                        plugins: ['babel-plugin-styled-components']
-                    }
-                }
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-                exclude: /node_modules/
-            },
-            {
-                test: /\.svg$/,
-                use: [
-                    {
-                        loader: '@svgr/webpack',
-                        options: {
-                            icon: true,
-                        },
-                    },
-                ],
-            }
-        ]
-    },
-    plugins: [
-        new CopyWebpackPlugin({
-            patterns: [
-                {from: 'src/main/resources/frontend', to: ''}
-            ]
-        })
-    ],
-    devServer: {
-        historyApiFallback: true,
-        static: {
-            directory: path.join(__dirname, 'dist'),
+module.exports = (env, argv) => {
+
+    const isProd = argv.mode === 'production';
+
+    return {
+        mode: argv.mode ?? 'production',
+        entry: './src/main/tsx/index.tsx',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.js'
         },
-        compress: true,
-        port: 3000,
-        proxy: [
-            {
-                context: ['/api'],
-                target: 'http://localhost:8080',
-                secure: false
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+            alias: {
+                '@': path.resolve(__dirname, 'src/main/tsx')
             }
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env', '@babel/preset-react'],
+                            plugins: ['babel-plugin-styled-components']
+                        }
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader'],
+                    exclude: /node_modules/
+                },
+            ]
+        },
+        plugins: [
+            ...(isProd ? [] : [
+                new CopyWebpackPlugin({
+                    patterns: [
+                        {from: 'src/main/resources/frontend', to: ''}
+                    ]
+                })
+            ]),
         ],
-        hot: true
-    }
+        devtool: isProd ? 'source-map' : 'eval-source-map',
+        optimization: {
+            minimize: isProd,
+        },
+        devServer: isProd ? undefined : {
+            historyApiFallback: true,
+            static: {
+                directory: path.join(__dirname, 'dist'),
+            },
+            compress: true,
+            port: 3000,
+            proxy: [
+                {
+                    context: ['/api'],
+                    target: 'http://localhost:8080',
+                    secure: false
+                }
+            ],
+            hot: true,
+        }
+    };
+
 };
