ba89cd9a2bbec7995f76f7a549d39da0ac1071e3
Lennart Möller <kontakt@lennartmoeller.com>
2025-03-27 21:13:19 +0100

    Add test class for YearMonthConverter

--- backend/src/main/java/com/lennartmoeller/finance/repository/CategoryRepository.java
+++ backend/src/main/java/com/lennartmoeller/finance/repository/CategoryRepository.java
@@ -3,7 +3,6 @@ package com.lennartmoeller.finance.repository;
 import com.lennartmoeller.finance.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
