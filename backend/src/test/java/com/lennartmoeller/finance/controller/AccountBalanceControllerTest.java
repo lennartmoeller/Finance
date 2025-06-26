c8144996a34712445d3b635a2853748f35e6b46a
Lennart Möller <59568564+lennartmoeller@users.noreply.github.com>
2025-06-26 15:48:18 +0200

    Add controller unit tests

---
+++ backend/src/test/java/com/lennartmoeller/finance/controller/AccountBalanceControllerTest.java
@@ -0,0 +1,34 @@
+package com.lennartmoeller.finance.controller;
+
+import com.lennartmoeller.finance.dto.AccountBalanceDTO;
+import com.lennartmoeller.finance.service.AccountBalanceService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class AccountBalanceControllerTest {
+
+    private AccountBalanceService service;
+    private AccountBalanceController controller;
+
+    @BeforeEach
+    void setUp() {
+        service = mock(AccountBalanceService.class);
+        controller = new AccountBalanceController(service);
+    }
+
+    @Test
+    void testGetAccountBalances() {
+        List<AccountBalanceDTO> list = List.of(new AccountBalanceDTO(), new AccountBalanceDTO());
+        when(service.findAll()).thenReturn(list);
+
+        List<AccountBalanceDTO> result = controller.getAccountBalances();
+
+        assertEquals(list, result);
+        verify(service).findAll();
+    }
+}
