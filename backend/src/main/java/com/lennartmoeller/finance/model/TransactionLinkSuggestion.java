15457034a27145ec99cefc2999ea348dc7bf547d
Lennart Möller <59568564+lennartmoeller@users.noreply.github.com>
2025-07-05 12:12:03 +0200

    feat: add transaction link suggestion entity

---
+++ backend/src/main/java/com/lennartmoeller/finance/model/TransactionLinkSuggestion.java
@@ -0,0 +1,28 @@
+package com.lennartmoeller.finance.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@Entity
+@EqualsAndHashCode(of = "id", callSuper = false)
+@RequiredArgsConstructor
+@Table(name = "transaction_link_suggestions")
+public class TransactionLinkSuggestion extends BaseModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "bank_transaction", nullable = false)
+    private BankTransaction bankTransaction;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "transaction", nullable = false)
+    private Transaction transaction;
+
+    @Column(nullable = false)
+    private Double probability;
+}
