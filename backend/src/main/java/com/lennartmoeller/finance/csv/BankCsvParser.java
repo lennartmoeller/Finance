d39a843f88d457ed937d16fc432fd44cc0585368
Lennart Möller <kontakt@lennartmoeller.com>
2025-07-08 14:24:11 +0200

    refactor: remove nonnull annotation

--- backend/src/main/java/com/lennartmoeller/finance/csv/BankCsvParser.java
+++ backend/src/main/java/com/lennartmoeller/finance/csv/BankCsvParser.java
@@ -3,9 +3,7 @@ package com.lennartmoeller.finance.csv;
 import com.lennartmoeller.finance.dto.BankTransactionDTO;
 import java.io.InputStream;
 import java.util.List;
-import javax.annotation.Nonnull;
 
 public interface BankCsvParser<T extends BankTransactionDTO> {
-    @Nonnull
-    List<T> parse(@Nonnull InputStream inputStream);
+    List<T> parse(InputStream inputStream);
 }
