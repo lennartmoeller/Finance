6178eebbdf17ce2d01951c580446241f462b5f6e
Lennart Möller <kontakt@lennartmoeller.com>
2025-09-28 00:31:25 +0200

    fix: revert

--- frontend/src/components/Table/Table.tsx
+++ frontend/src/components/Table/Table.tsx
@@ -1,9 +1,6 @@
-import React, { ReactNode, useRef } from "react";
-
-import { useVirtualizer } from "@tanstack/react-virtual";
+import React, { ReactNode } from "react";
 
 import StyledTable from "@/components/Table/styles/StyledTable";
-import { memo } from "@/utils/react";
 
 interface TableProps<T> {
     data: Array<T>;
@@ -11,74 +8,19 @@ interface TableProps<T> {
     body: (element: T, index: number) => ReactNode;
     preRow?: ReactNode;
     postRow?: ReactNode;
-    containerHeight?: number;
 }
 
-interface TableHeaderProps {
-    header: ReactNode;
-}
-
-function TableHeaderComponent({ header }: Readonly<TableHeaderProps>) {
-    return <thead>{header}</thead>;
-}
-const TableHeader = memo(TableHeaderComponent, "TableHeader");
-
-function TableComponent<T>({
-    data,
-    header,
-    body,
-    preRow,
-    postRow,
-    containerHeight = 400,
-}: Readonly<TableProps<T>>) {
-    const parentRef = useRef<HTMLDivElement>(null);
-
-    const virtualizer = useVirtualizer({
-        count: data.length,
-        getScrollElement: () => parentRef.current,
-        estimateSize: () => 50,
-    });
-
+const Table = <T,>({ data, header, body, preRow, postRow }: TableProps<T>) => {
     return (
-        <div
-            ref={parentRef}
-            style={{
-                height: `${containerHeight}px`,
-                overflow: "auto",
-            }}
-        >
-            <StyledTable>
-                <TableHeader header={header} />
-                <tbody
-                    style={{
-                        height: `${virtualizer.getTotalSize()}px`,
-                        position: "relative",
-                    }}
-                >
-                    {preRow}
-                    {virtualizer.getVirtualItems().map((virtualItem) => (
-                        <div
-                            key={virtualItem.key}
-                            data-index={virtualItem.index}
-                            ref={virtualizer.measureElement}
-                            style={{
-                                position: "absolute",
-                                top: 0,
-                                left: 0,
-                                width: "100%",
-                                transform: `translateY(${virtualItem.start}px)`,
-                                display: "table-row-group",
-                            }}
-                        >
-                            {body(data[virtualItem.index], virtualItem.index)}
-                        </div>
-                    ))}
-                    {postRow}
-                </tbody>
-            </StyledTable>
-        </div>
+        <StyledTable>
+            <thead>{header}</thead>
+            <tbody>
+                {preRow}
+                {data.map((element: T, index: number) => body(element, index))}
+                {postRow}
+            </tbody>
+        </StyledTable>
     );
-}
-const Table = memo(TableComponent, "Table");
+};
 
 export default Table;
--- frontend/src/views/TrackingView/TransactionsTable/TransactionsTable.tsx
+++ frontend/src/views/TrackingView/TransactionsTable/TransactionsTable.tsx
@@ -49,7 +49,6 @@ const TransactionsTable: React.FC<TransactionsTableProps> = ({
         <StyledTransactionTable>
             <Table
                 data={transactions}
-                containerHeight={600}
                 header={
                     <TableRow>
                         <TableHeaderCell sticky="top">Date</TableHeaderCell>
